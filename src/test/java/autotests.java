a62c3e0cadf8cc2a35d56ef0855ecbf5e5e484fd
Mikhail Lyubeznov <xelysqgbhju@gmail.com>
2023-03-12 13:15:24 +0300

    qwqwqdfbfb

--- src/test/java/autotests.java
+++ src/test/java/autotests.java
@@ -2,20 +2,38 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class autotests {
 
 
     @BeforeAll
     static void configure() {
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://passport.yandex.ru";
         Configuration.browserSize = "1920x1080";
         Configuration.holdBrowserOpen = true;
     }
 
  @Test
  void testOne() {
-     //шаг 1
-     //шаг 2
-
+     //шаг 1 открываем страницу
+     open("https://passport.yandex.ru/registration");
+     //шаг 2 поле имени
+     $("#firstname").setValue("Михаил");
+     //поле фамилии
+     $("#lastname").setValue("Любезнов");
+     //поле логина
+     $("#login").setValue("mihail123lubeznow");
+     //поле пароля
+     $("#password").setValue("Fh[fyutk123");
+     //поле повторения пароля
+     $("#password_confirm").setValue("Fh[fyutk123");
+     //клик по глазику
+     $(".field-type__toggler").click();
+     //номер мобильного телефона
+     $("#phone").setValue("9951896940");
+     //чекбокс соглашения на обработку данных
+     //кнопка регистрации
     }
 }
